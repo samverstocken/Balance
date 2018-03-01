0d74626e373ef3f0fd1bce646a2e164debac1e4a
Jeroen Duvillier <jeroen.duvillier@renson.be>
2018-03-01 19:14:09 +0100

    bla

--- parser.py
+++ parser.py
@@ -1,6 +1,14 @@
 import numpy as np
 from ride import ride
 from car import car
+import os
+
+filedir = os.getcwd()
+parentdir = os.path.split(filedir)[0]
+
+filename = "a_example"
+
+filename = os.path.join(parentdir, filename)
 
 
 def parser(filename):
@@ -38,4 +46,4 @@ def parser(filename):
 
     return rows, columns, vechicles, rides, bonus, steps, out
 
-
+parser(filename)
