f46759ae8609f9a8f7e18efe6a041da3bdee99ac
Sam Verstocken <sam.verstocken@ugent.be>
2018-03-01 20:41:01 +0100

    gege

--- car.py
+++ car.py
@@ -3,31 +3,53 @@ class Car:
 
     def __init__(self):
 
+        """
+        This function ...
+        """
+
         self.pos = Position(0, 0)
 
-        self.riding = False
-        self.rides = []
+        self.history = []
+        self.current = None
+        self.waiter = None
 
         self.totarget = False
 
-    @property
-    def nrides(self):
+        #self.disttotarget =
 
-        return len(self.rides)
+    @property
+    def riding(self):
+        return self.current is not None
+
+    @property
+    def waiting(self):
+        return self.waiting is not None
 
     def add_ride(self, ride):
-
-        self.rides.append(ride)
-        #self.riding = True
-
-    def check_ride(self):
-
-
+        self.waiter = ride
 
     @property
-    def last_ride(self):
-        return self.rides[-1]
+    def available(self):
+        return self.waiting is None and self.current is None
+
+    @property
+    def dist_to_finish(self):
+        return self.pos.distance(self.current.finish)
 
     def update(self):
+
+        if self.waiting: # WAITING
+
+            if self.pos == self.waiting.pos:
+
+
+            else:
+
+        elif self.riding: # DRIVING
+
+
+
+        else: pass
+
 
 #
