66c667d13c6dd76983614b19a1b5b1ca708fca4c
Sam Verstocken <sam.verstocken@ugent.be>
2018-03-01 19:23:29 +0100

    Tine

--- car.py
+++ car.py
@@ -1,8 +1,14 @@
-class car:
+class Car:
 
-	def __init__(self,riding):
+	def __init__(self):
 		self.pos = [0,0]
 		self.riding = false
+		self.rides = 0
+		self.ridelist = []
 
 	def setPos(pos):
 		self.start = start
+	def setRiding(riding,rideID):
+		self.riding = riding
+		self.rides += 1
+		self.ridelist.append(rideID)
